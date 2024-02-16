dad20b8b50f5b3f40c42e005e6530f4b92023a71
jofbrvega <jofbr.business@gmail.com>
2024-02-16 18:12:56 +0100

    Updating case according to  feedback - removing validation message english function

--- index.js
+++ index.js
@@ -31,37 +31,3 @@ function checkParentNameValue() {
   }
   return true;
 }
-
-/*  Custom validation messages in Swedish and English depending on page language */
-function setCustomValidityMessages() {
-  var lang = document.documentElement.lang;
-
-  if (lang === "sv") {
-    document
-      .getElementById("inputFirstName")
-      .setCustomValidity("Vänligen fyll i med ditt förnamn");
-    document
-      .getElementById("inputEmail")
-      .setCustomValidity("Vänligen ange en giltig e-postadress");
-    document
-      .getElementById("inputZipcode")
-      .setCustomValidity("Vänligen ange ett giltigt postnummer t.ex. 123 45");
-  } else {
-    // Default to English messages
-    document
-      .getElementById("inputFirstName")
-      .setCustomValidity("Please fill in with your first name");
-    document
-      .getElementById("inputEmail")
-      .setCustomValidity("Please enter a valid email address");
-    document
-      .getElementById("inputZipcode")
-      .setCustomValidity("Please enter a valid postal code e.g. 123 45");
-    document
-      .getElementById("inputParentName")
-      .setCustomValidity("Please enter a first name in the correct format A-Z");
-  }
-}
-
-// Call the function when the page loads
-window.onload = setCustomValidityMessages;
