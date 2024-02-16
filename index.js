a6f44f1d4d817b91cedcf77de0c18ce61c356317
jofbrvega <jofbr.business@gmail.com>
2024-02-16 18:29:01 +0100

    Correcting javascript function checkparentnamevalue

--- index.js
+++ index.js
@@ -13,21 +13,25 @@ function showInformation() {
 
 // Function that checks that inputParentName is not the same value as inputFirstName & that it contains atleast a letter between A-Z
 function checkParentNameValue() {
-  var firstName = document.getElementById("inputFirstName").value
-  var parentName = document.getElementById("inputParentName").value;
+  var firstName = document.getElementById("inputFirstName").value.toLowerCase();
+  var parentName = document
+    .getElementById("inputParentName")
+    .value.toLowerCase();
 
   if (firstName == parentName) {
     document
-    .getElementById("inputParentName")
-    .setCustomValidity("Förnamn på förälder får inte vara samma som ditt förnamn");
+      .getElementById("inputParentName")
+      .setCustomValidity(
+        "Förnamn på förälder får inte vara samma som ditt förnamn"
+      );
     return false;
   }
-  var parentNamePattern = /^[a-zA-Z]+$/ ;
+  var parentNamePattern = /^[a-zA-Z]+$/;
   if (!parentNamePattern.test(parentName)) {
     document
       .getElementById("inputParentName")
       .setCustomValidity("Vänligen ange förnamn i rätt format A-Z");
-      return false;
+    return false;
   }
   return true;
 }
